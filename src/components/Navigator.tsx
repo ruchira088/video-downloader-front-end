06bb4822bf03a8a588f238fd1e06df58463b7964
Ruchira (Richie) Jayasekara <me@ruchij.com>
2020-05-27 00:19:57 +1000

    Creating the navigator programmatically

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -1,28 +1,31 @@
 import React from "react"
 import {Link} from "react-router-dom"
 import {Grid} from "@material-ui/core";
+import styles from "./Navigator.module.css"
+
+interface NavigationTab {
+    label: string
+    path: string
+}
+
+const navigationTabs: NavigationTab[] = [
+        {label: "Videos", path: "/"},
+        {label: "Schedule", path: "/schedule"},
+        {label: "Active Downloads", path: "/active"},
+        {label: "Service Information", path: "/service-information"}
+    ]
 
 export default () => (
-    <Grid container className="navigator">
-        <Grid item xs={3}>
-            <Link to="/">
-                <div className="navigation-tab-name">Videos</div>
-            </Link>
-        </Grid>
-        <Grid item xs={3}>
-            <Link to="/schedule">
-                <div className="navigation-tab-name">Schedule</div>
-            </Link>
-        </Grid>
-        <Grid item xs={3}>
-            <Link to="/active">
-                <div className="navigation-tab-name">Active Downloads</div>
-            </Link>
-        </Grid>
-        <Grid item xs={3}>
-            <Link to="/service-information">
-                <div className="navigation-tab-name">Service Information</div>
-            </Link>
-        </Grid>
+    <Grid container className={styles.navigator}>
+        {
+            navigationTabs
+                .map((navigationTab, index) =>
+                    <Grid item xs={3} className={styles.navigatorTab} key={index}>
+                        <Link to={navigationTab.path}>
+                            <div>{navigationTab.label}</div>
+                        </Link>
+                    </Grid>
+                )
+        }
     </Grid>
 )
