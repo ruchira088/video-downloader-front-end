5fea73872333be0c8ae643259ee76f0b092ca72b
Ruchira (Richie) Jayasekara <me@ruchij.com>
2020-12-01 19:41:58 +1100

    Updating the release script

--- src/services/http/HttpClient.ts
+++ src/services/http/HttpClient.ts
@@ -1,24 +1,23 @@
-import axios, {AxiosInstance} from "axios"
-import {configuration} from "services/Configuration"
-import {KeySpace, LocalKeyValueStore} from "../kv-store/KeyValueStore";
-import {AuthenticationKey} from "../authentication/AuthenticationService";
+import axios, { AxiosInstance } from "axios"
+import { configuration } from "services/Configuration"
+import { KeySpace, LocalKeyValueStore } from "../kv-store/KeyValueStore"
+import { AuthenticationKey } from "../authentication/AuthenticationService"
 
 export const axiosClient: AxiosInstance = axios.create({
   baseURL: configuration.apiService,
-  withCredentials: true
+  withCredentials: true,
 })
 
 axiosClient.interceptors.response.use(
-    value => Promise.resolve(value),
-      error => {
+  (value) => Promise.resolve(value),
+  (error) => {
+    if (error.response.status === 401) {
+      const keyValueStore = new LocalKeyValueStore(KeySpace.Authentication)
+      keyValueStore.remove(AuthenticationKey.Token)
 
-        if (error.response.status === 401) {
-          const  keyValueStore = new LocalKeyValueStore(KeySpace.Authentication)
-          keyValueStore.remove(AuthenticationKey.Token)
+      window.location.reload()
+    }
 
-          window.location.reload()
-        }
-
-        return Promise.reject(error)
-      }
+    return Promise.reject(error)
+  }
 )
