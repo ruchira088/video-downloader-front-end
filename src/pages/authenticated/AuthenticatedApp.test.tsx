0e8aaeb349b119a74be3faba0dc1b240edbb4cd1
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-07-16 02:26:32 +1000

    Updating the K8s deployment Docker image

--- src/pages/authenticated/AuthenticatedApp.test.tsx
+++ src/pages/authenticated/AuthenticatedApp.test.tsx
@@ -1,9 +1,9 @@
 import React from "react"
 import { render } from "@testing-library/react"
-import App from "./AuthenticatedApp"
+import AuthenticatedApp from "./AuthenticatedApp"
 
 test("renders learn react link", () => {
-  const { getByText } = render(<App />)
+  const { getByText } = render(<AuthenticatedApp />)
   const linkElement = getByText(/pending/i)
   expect(linkElement).toBeInTheDocument()
 })
