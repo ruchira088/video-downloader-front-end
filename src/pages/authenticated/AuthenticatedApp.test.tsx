39b2b5c79058a962d488652d0b76be4aaac768e5
Ruchira (Richie) Jayasekara <me@ruchij.com>
2023-08-21 20:53:57 +1000

    Upgrading dependencies

--- src/pages/authenticated/AuthenticatedApp.test.tsx
+++ src/pages/authenticated/AuthenticatedApp.test.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { render } from "@testing-library/react"
+import "@testing-library/jest-dom"
 import AuthenticatedApp from "./AuthenticatedApp"
 
 test("renders learn react link", () => {
