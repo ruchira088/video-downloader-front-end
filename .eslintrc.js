9cb8f58c39592fe06f63bbe9f3a6de932f0e8167
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-11-05 01:12:23 +1100

    Upgrading dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   rules: {
     "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/ban-types": "off"
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/no-empty-object-type": "off"
   },
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
 }
