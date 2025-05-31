9d668c93b77bd3a19cf9f83c0f3d413bbaa36c1a
Ruchira (Richie) Jayasekara <me@ruchij.com>
2025-05-31 17:33:08 +1000

    Add HistoryPage component with infinite scroll and video history display; introduce styles for layout

--- app/pages/authenticated/history/HistoryPage.tsx
+++ app/pages/authenticated/history/HistoryPage.tsx
@@ -1,44 +1,75 @@
-import React, { useEffect, useState } from "react"
-import { getVideoHistory } from "~/services/history/HistoryService"
-import { VideoWatchHistory } from "~/models/VideoWatchHistory"
+import React, {useEffect, useRef, useState} from "react"
+import {Link} from "react-router"
+import {getVideoHistory} from "~/services/history/HistoryService"
+import {VideoWatchHistory} from "~/models/VideoWatchHistory"
 import VideoCard from "~/components/video/video-card/VideoCard"
-import { Link } from "react-router"
+
+import styles from "./HistoryPage.module.scss"
+import InfiniteScroll from "~/components/infinite-scroll/InfiniteScroll"
+
+const PAGE_SIZE = 50
 
 const HistoryPage = () => {
   const [videoWatchHistories, setVideoWatchHistories] = useState<VideoWatchHistory[]>([])
+  const [pageNumber, setPageNumber] = useState(0)
+  const videoIds = useRef<Set<string>>(new Set())
+  const isLoading = useRef(false)
+  const hasMore = useRef(true)
+
+  const retrieveVideoHistory = async () => {
+    isLoading.current = true
+
+    try {
+      const videoHistories: VideoWatchHistory[] = await getVideoHistory(pageNumber, PAGE_SIZE)
+
+      if (videoHistories.length < PAGE_SIZE) {
+        hasMore.current = false
+      }
+
+      const newVideoHistories = videoHistories.reduce<VideoWatchHistory[]>(
+        (videos, videoHistory) => {
+          if (!videoIds.current.has(videoHistory.video.videoMetadata.id)) {
+            videoIds.current.add(videoHistory.video.videoMetadata.id)
+            return videos.concat(videoHistory)
+          } else {
+            return videos
+          }
+        },
+        []
+      )
+
+      setVideoWatchHistories(videoWatchHistories => videoWatchHistories.concat(newVideoHistories))
+    } finally {
+      isLoading.current = false
+    }
+  }
 
   useEffect(() => {
-    getVideoHistory(0, 50)
-      .then(values => {
-        const videoIds = new Set()
-        const watchedItems: VideoWatchHistory[] = []
-
-        values.forEach(videoWatchHistory => {
-          if (!videoIds.has(videoWatchHistory.video.videoMetadata.id)) {
-            videoIds.add(videoWatchHistory.video.videoMetadata.id)
-            watchedItems.push(videoWatchHistory)
-          }
-        })
-
-        setVideoWatchHistories(watchedItems)
-      })
-  }, [])
+    retrieveVideoHistory()
+  }, [pageNumber])
 
   return (
     <div>
       {/*<Helmet>*/}
       {/*  <title>History</title>*/}
       {/*</Helmet>*/}
-      <div>
+      <InfiniteScroll
+        loadMore={() => setPageNumber(pageNumber => pageNumber + 1)}
+        isLoading={isLoading.current}
+        hasMore={hasMore.current}
+        className={styles.videoHistoryGallery}
+      >
         {
           videoWatchHistories.map(
             (videoWatchHistory, index) =>
-              <Link to={`/video/${videoWatchHistory.video.videoMetadata.id}`} key={index}>
-                <VideoCard video={videoWatchHistory.video}/>
-              </Link>
+              <div key={index} className={styles.videoHistoryCard}>
+                <Link to={`/video/${videoWatchHistory.video.videoMetadata.id}`}>
+                  <VideoCard video={videoWatchHistory.video}/>
+                </Link>
+              </div>
           )
         }
-      </div>
+      </InfiniteScroll>
     </div>
   )
 
