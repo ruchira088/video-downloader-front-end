b4e5a911674f3fe111046be944068a30ef1d6e5f
Ruchira (Richie) Jayasekara <me@ruchij.com>
2025-06-08 02:48:16 +1000

    Refactor LoadableComponent to remove unused styles and classNames dependency

--- app/components/hoc/loading/LoadableComponent.tsx
+++ app/components/hoc/loading/LoadableComponent.tsx
@@ -1,9 +1,6 @@
 import React, {type FC, type ReactNode} from "react"
 import {Option} from "~/types/Option"
-
-import styles from "./LoadableComponent.module.scss"
 import {CircularProgress} from "@mui/material"
-import classNames from "classnames"
 
 type LoadableComponentProps = {
   readonly children: Option<ReactNode>
@@ -11,7 +8,7 @@ type LoadableComponentProps = {
 }
 
 export const LoadableComponent: FC<LoadableComponentProps> = props =>
-  <div className={classNames(styles.loadableComponent, props.className)}>
+  <div className={props.className}>
     {
       props.children.fold<ReactNode>(
         () => <CircularProgress size="5em"/>,
