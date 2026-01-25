2b973e0eaf0439a6a8cfec67b85c4572fc53dd86
Ruchira (Richie) Jayasekara <me@ruchij.com>
2026-01-26 02:29:03 +1100

    Update Sentry DSN mappings for all environments (Development, Staging, Production).

--- app/services/Sentry.ts
+++ app/services/Sentry.ts
@@ -2,9 +2,9 @@ import * as Sentry from "@sentry/react"
 import { Environment, getEnvironment } from "~/services/Config"
 
 const DSN_MAPPINGS: Record<Environment, string> = {
-  [Environment.Development]: "https://294ffe65738b62400211626678d54021@o4510770741837824.ingest.us.sentry.io/4510771405455360",
-  [Environment.Staging]: "https://294ffe65738b62400211626678d54021@o4510770741837824.ingest.us.sentry.io/4510771405455360",
-  [Environment.Production]: "https://294ffe65738b62400211626678d54021@o4510770741837824.ingest.us.sentry.io/4510771405455360"
+  [Environment.Development]: "https://3cefa278484696cde7ff9e066525fa87@o4510770741837824.ingest.us.sentry.io/4510771763085312",
+  [Environment.Staging]: "https://0c16af37660ae085a000b8b8f9d79a17@o4510770741837824.ingest.us.sentry.io/4510771760791552",
+  [Environment.Production]: "https://77f336ea08b08c576b93c76458db362f@o4510770741837824.ingest.us.sentry.io/4510771752992768"
 }
 
 const getDsn = () => {
